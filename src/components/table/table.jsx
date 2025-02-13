b42429fc546fb1010b539514c35591e3cdf26270
Arnold Dadayan <adadayan@servicetitan.com>
2025-02-13 15:05:15 +0400

    movie details improvments

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -1,4 +1,11 @@
+import imdbLogo from "../../assets/images/imdb.svg";
+
 export const Table = ({ data, onRowClick }) => {
+  const handleOpenIMDBbMovie = (event, imdbID) => {
+    event.stopPropagation();
+    window.open(`https://www.imdb.com/title/${imdbID}`, " ");
+  };
+
   return (
     <table className="table table-striped mt-3 table-hover">
       <thead className="thead-dark">
@@ -19,7 +26,18 @@ export const Table = ({ data, onRowClick }) => {
             <td>{movie.Title}</td>
             <td>{movie.Year}</td>
             <td>{movie.Type}</td>
-            <td className="text-md-end">{movie.imdbID}</td>
+            <td className="text-md-end">
+              <div className="d-flex justify-content-end">
+                <button
+                  type="button"
+                  className="btn btn-link"
+                  onClick={(event) => handleOpenIMDBbMovie(event, movie.imdbID)}
+                >
+                  Watch on IMDB
+                  <img src={imdbLogo} alt="" width={20} />
+                </button>
+              </div>
+            </td>
           </tr>
         ))}
       </tbody>
--- src/pages/search-movies/movie-details/movie-details.jsx
+++ src/pages/search-movies/movie-details/movie-details.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { omdbApi } from "../../../api/movie.api";
 import { useLocalStorageState } from "../../../hooks/use-local-storage-state";
+import { Flag } from "../../../components/flag/flag";
 
 export const MovieDetails = ({ id }) => {
   const [movie, setMovie] = useState({});
@@ -48,6 +49,8 @@ export const MovieDetails = ({ id }) => {
     }
   };
 
+  console.log(movie.Country);
+
   return (
     <div>
       <div className="d-flex justify-content-between">
@@ -58,6 +61,19 @@ export const MovieDetails = ({ id }) => {
             width={300}
             className="h-auto rounded d-block"
           />
+          <ul className="list-group list-group-flush">
+            {(movie?.Ratings || []).map((rating, index) => (
+              <li
+                key={rating.Source}
+                className="list-group-item d-flex justify-content-between align-items-center"
+              >
+                {rating.Source}
+                <span className="badge text-bg-primary rounded-pill">
+                  {rating.Value}
+                </span>
+              </li>
+            ))}
+          </ul>
         </div>
         <div>
           <p className="text-gray-600 d-flex align-items-center justify-content-between">
@@ -92,6 +108,30 @@ export const MovieDetails = ({ id }) => {
           </p>
           <p className="text-gray-600">
             <strong>Language:</strong> {movie.Language}
+          </p>
+          <p className="text-gray-600">
+            <strong>Countries:</strong>
+            {(movie.Country || "").split(", ").map((country, index) => (
+              <Flag key={country} country={country} />
+            ))}
+          </p>
+          <p className="text-gray-600">
+            <strong>Released:</strong> {movie.Released}
+          </p>
+          <p className="text-gray-600">
+            <strong>Runtime:</strong> {movie.Runtime}
+          </p>
+          <p className="text-gray-600">
+            <strong>IMDb Votes :</strong> {movie.imdbRating} ({movie.imdbVotes}{" "}
+            votes)
+          </p>
+
+          <p className="mt-4 text-gray-700">{movie.Plot}</p>
+          <p className="mt-2 text-gray-600">
+            <strong>Box Office:</strong> {movie.BoxOffice}
+          </p>
+          <p className="mt-2 text-gray-600">
+            <strong>Awards:</strong> {movie.Awards}
           </p>
         </div>
       </div>
